568481a4abb1dc4ca36a71e977e2c4ab9181f7f5
SCO-Techlab <sco.techlab@gmail.com>
2024-09-23 12:26:35 +0200

    add external translates example

---
+++ src/translates.mock.ts
@@ -0,0 +1,39 @@
+// This TRANSLATES_MOCK const variable simulate data you can get from DB for example
+
+export const TRANSLATES_MOCK: any = {
+    en: {
+        "hello.world": "Hello world",
+        "tests": {
+            "test1": {
+                "1": "First translate of tests / test1 block"
+            }
+        }
+    },
+
+    es: {
+        "hello.world": "Hola mundo",
+        "tests": {
+            "test1": {
+                "1": "Primera traducción del bloque tests / test1"
+            }
+        }
+    },
+
+    it: {
+        "hello.world": "Ciao mondo",
+        "tests": {
+            "test1": {
+                "1": "Prima traduzione di blocco tests / test1"
+            }
+        }
+    },
+
+    pl: {
+        "hello.world": "Witaj świecie",
+        "tests": {
+            "test1": {
+                "1": "Pierwsze tłumaczenie bloku tests / test1"
+            }
+        }
+    },
+};
